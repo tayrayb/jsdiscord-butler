2e36d012ed9ceb5c2bfc50e8c456405dce550cf3
Tayray <bush.taylor@gmail.com>
2018-07-05 19:02:38 +1000

    Update Bot, added basic gif search

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const conf = require('./conf.js');
 const Giphy = require('giphy-api')(conf.giphyAPI);
-var prefix = '!'
+var prefix = conf.prefix;
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = conf.discordAPI;
@@ -18,8 +18,32 @@ bot.on('message', message => {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
-
-    
+    //Ping pong!
+    if (cmd === `${prefix}ping`) {
+        message.channel.send('pong')
+        return;
+    }
+    //Gif Commands
+    if (cmd === `${prefix}gif`) {
+        if (args[0] === 'random') {
+            Giphy.random({
+                rating: 'g',
+                fmt: 'json'
+            }, function (err, res) {
+                message.channel.send(res.data.url);
+                console.log(res.data.url);
+            })
+        } else if (args[0] === 'search'){
+            Giphy.search({
+                q: args[1],
+                fmt: 'json',
+                limit: 1
+            }, function (err, res) {
+                message.channel.send(res.data[0].url);
+                console.log(res.data[0].url);
+                return;
+            });
+        }
     }
 })
 bot.login(token)
