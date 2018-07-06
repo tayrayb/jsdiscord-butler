cd3b264cd810c3db2a2b6ddf6dcf8446ad52ed48
BuildTools <bush.taylor@gmail.com>
2018-07-06 16:33:11 +1000

    Update

--- bot.js
+++ bot.js
@@ -2,6 +2,10 @@
 const conf = require('./conf.js');
 const Giphy = require('giphy-api')(conf.giphyAPI);
 const Discord = require('discord.js');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('ffmpeg-binaries');
+
 //Prefix
 var prefix = conf.prefix;
 const talkedRecently = new Set();
@@ -18,7 +22,7 @@ bot.on('ready', () => {
 bot.on('message', message => {
     //Timeout check
     if (talkedRecently.has(message.author.id))
-    return;
+        return;
     //Add user to timeout
     talkedRecently.add(message.author.id);
     setTimeout(() => {
@@ -37,12 +41,38 @@ bot.on('message', message => {
         message.channel.send('pong')
         return;
     }
+    //Youtube music
+    if (cmd === `${prefix}yt`) {
+        if (args[0] === 'stop') {
+            message.member.voiceChannel.leave;
+            fs.exists('./music.mp3', function (exists) {
+                if (exists) {
+                    fs.unlinkSync('./music.mp3')
+                }
+            })
+        }
+        //(youtube.com)d
+        if (args[0]) {
+            message.channel.send('Downloading Youtube video');
+            ytdl(args[0]).pipe(fs.createWriteStream('./music.mp3', { autoClose: true },).on('close', function () {
+                message.channel.send('Finished: Downloadling Youtube Link');
+                if (message.member.voiceChannel) {
+                    message.member.voiceChannel.join()
+                        .then(connection => {
+                            message.reply('I have joined voice!');
+                            connection.playFile('./music.mp3')
+                        })
+                }
+            }));
+        } else {
+            message.channel.send('Please enter Youtube URL')
+        }
+    }
     //Avatar commands
     if (cmd === `${prefix}avatar`) {
         if (args[0]) {
             message.channel.send(dUser.avatarURL)
-        }
-        else {
+        } else {
             message.channel.send(message.author.avatarURL)
         }
     }
