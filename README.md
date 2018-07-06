5b491943402742e4b6b129738b7ac9af00542b62
Tayray <bush.taylor@gmail.com>
2018-07-06 20:09:50 +1000

    Add basic youtube player

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
 # jsdiscord-butler
 Discord Bot written in Javascript
 
-You must make a conf.js file and paste in your API keys for bot funtionality. Copy and paste code block below.
-
-```
-module.exports = {
-        discordAPI: '',
-        giphyAPI: ''
-}
-```
-
-API Keys are now redundant, don't bother trying to get in again.
+Simply run the bat file.
--- bot.js
+++ bot.js
@@ -1,104 +1,96 @@
-//Imports
-const conf = require('./conf.js');
-const Giphy = require('giphy-api')(conf.giphyAPI);
-const Discord = require('discord.js');
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-const ffmpeg = require('ffmpeg-binaries');
-
-//Prefix
-var prefix = conf.prefix;
-const talkedRecently = new Set();
-//Discord Constants
-const bot = new Discord.Client();
-const token = conf.discordAPI;
-
-//Command Ready
+// Imports
+const conf = require('./conf.js')
+const Giphy = require('giphy-api')(conf.giphyAPI)
+const Discord = require('discord.js')
+const ytdl = require('ytdl-core')
+// Prefix
+var prefix = conf.prefix
+const talkedRecently = new Set()
+// Discord Constants
+const bot = new Discord.Client()
+const token = conf.discordAPI
+// Command Ready
 bot.on('ready', () => {
-    console.log(`${bot.user.username} is online!`);
-    bot.user.setActivity("Being Built");
-});
-//Start of Bot
-bot.on('message', message => {
-    //Timeout check
-    if (talkedRecently.has(message.author.id))
-        return;
-    //Add user to timeout
-    talkedRecently.add(message.author.id);
-    setTimeout(() => {
-        talkedRecently.delete(message.author.id);
-    }, 2500);
-    //Prevent bot from talking to itself
-    if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
-    //Set cmd and arrays
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
-    let dUser = message.mentions.users.first();
-    //Ping pong!
-    if (cmd === `${prefix}ping`) {
-        message.channel.send('pong')
-        return;
-    }
-    //Youtube music
-    if (cmd === `${prefix}yt`) {
-        if (args[0] === 'stop') {
-            message.member.voiceChannel.leave;
-            fs.exists('./music.mp3', function (exists) {
-                if (exists) {
-                    fs.unlinkSync('./music.mp3')
-                }
-            })
-        }
-        //(youtube.com)d
-        if (args[0]) {
-            message.channel.send('Downloading Youtube video');
-            ytdl(args[0]).pipe(fs.createWriteStream('./music.mp3', { autoClose: true },).on('close', function () {
-                message.channel.send('Finished: Downloadling Youtube Link');
-                if (message.member.voiceChannel) {
-                    message.member.voiceChannel.join()
-                        .then(connection => {
-                            message.reply('I have joined voice!');
-                            connection.playFile('./music.mp3')
-                        })
-                }
-            }));
-        } else {
-            message.channel.send('Please enter Youtube URL')
-        }
-    }
-    //Avatar commands
-    if (cmd === `${prefix}avatar`) {
-        if (args[0]) {
-            message.channel.send(dUser.avatarURL)
-        } else {
-            message.channel.send(message.author.avatarURL)
-        }
-    }
-    //Gif Commands
-    if (cmd === `${prefix}gif`) {
-        if (args[0] === 'random') {
-            Giphy.random({
-                rating: 'g',
-                fmt: 'json'
-            }, function (err, res) {
-                message.channel.send(res.data.url);
-                console.log(res.data.url);
-                return;
-            })
-        } else if (args[0] === 'search') {
-            Giphy.random({
-                tag: args[1],
-                rating: 'g',
-                fmt: 'json'
-            }, function (err, res) {
-                message.channel.send(res.data.url);
-                console.log(res.data.url);
-                return;
-            });
-        }
-    }
+  console.log(`${bot.user.username} is online!`)
+  bot.user.setActivity('Being Built')
 })
-//Bot Login
+// Start of Bot
+bot.on('message', message => {
+  // Timeout check
+  if (talkedRecently.has(message.author.id)) { return }
+  // Add user to timeout
+  talkedRecently.add(message.author.id)
+  setTimeout(() => {
+    talkedRecently.delete(message.author.id)
+  }, 2500)
+  // Prevent bot from talking to itself
+  if (message.author.bot) return
+  if (message.channel.type === 'dm') return
+  // Set cmd and arrays
+  let messageArray = message.content.split(' ')
+  let cmd = messageArray[0]
+  let args = messageArray.slice(1)
+  let dUser = message.mentions.users.first()
+  // Ping pong!
+  if (cmd === `${prefix}ping`) {
+    message.channel.send('pong')
+    return
+  }
+  // Youtube music
+  if (cmd === `${prefix}yt`) {
+    if (ytdl.validateURL(args[0])) {
+      if (message.member.voiceChannel) {
+        message.member.voiceChannel.join()
+          .then(connection => {
+            message.channel.send('Successfully joined voice channel and will now play')
+            connection.playStream(ytdl(args[0]))
+          }).catch(console.log)
+      } else {
+        message.channel.send('You need to join an channel first!')
+      }
+    }
+    if (!args[0]) {
+      message.channel.send('Please provide a Youtube link')
+      return
+    }
+    if (args[0] === 'stop') {
+      if (message.guild.voiceConnection) {
+        message.member.voiceChannel.leave()
+        message.channel.send('Leaving voice channel')
+      } else {
+        message.channel.send('I have already left')
+      }
+    }
+  }
+  // Avatar commands
+  if (cmd === `${prefix}avatar`) {
+    if (args[0]) {
+      message.channel.send(dUser.avatarURL)
+    } else {
+      message.channel.send(message.author.avatarURL)
+    }
+  }
+  // Gif Commands
+  if (cmd === `${prefix}gif`) {
+    if (args[0] === 'random') {
+      Giphy.random({
+        rating: 'g',
+        fmt: 'json'
+      }, function (res) {
+        message.channel.send(res.data.url)
+        console.log(res.data.url)
+      })
+    } else if (args[0] === 'search') {
+      Giphy.random({
+        tag: args[1],
+        rating: 'g',
+        fmt: 'json'
+      }, function (res) {
+        message.channel.send(res.data.url)
+        console.log(res.data.url)
+      })
+    }
+  }
+})
+// Bot Login
 bot.login(token)
