c076afad2cf84f90fe2c94f9d108892e3ffaaf3b
ElectrcNINJA <electrc@live.com>
2018-07-07 16:10:47 -0500

    check for env.vars

--- bot.js
+++ bot.js
@@ -1,103 +1,122 @@
+if (!haveVars) {
+    return;
+}
+
 // Imports
 const Giphy = require('giphy-api')(process.env.GIPHY_KEY)
 const Discord = require('discord.js')
 const ytdl = require('ytdl-core')
 // Prefix
-var prefix = process.env.PREFIX|| ';;' // Look in config file for user specified prefix... if none set to ';;'
+var prefix = process.env.PREFIX || ';;' // Look in config file for user specified prefix... if none set to ';;'
 const talkedRecently = new Set()
 // Discord Constants
 const bot = new Discord.Client()
 const token = process.env.DISCORD_KEY; //Set the Token to what is in the config file, if not set try the env varible 'APIKEY'.
-if (!token) {
-    console.log('Please Provide a Discord API Token "DSKEY" in enviroment varibles ')
-    return;
-}
-if (!prefix) {
-    console.log('Please Provide a prefic "PREFIX" in enviroment varibles ')
-    return;
-}
+
+
 // Command Ready
 bot.on('ready', () => {
-  console.log(`${bot.user.username} is online!`)
-  bot.user.setActivity('Being Built')
+    console.log(`${bot.user.username} is online!`)
+    bot.user.setActivity('Being Built')
 })
 // Start of Bot
 bot.on('message', message => {
-  // Timeout check
-  if (talkedRecently.has(message.author.id)) { return }
-  // Add user to timeout
-  talkedRecently.add(message.author.id)
-  setTimeout(() => {
-    talkedRecently.delete(message.author.id)
-  }, 2500)
-  // Prevent bot from talking to itself
-  if (message.author.bot) return
-  if (message.channel.type === 'dm') return
-  // Set cmd and arrays
-  let messageArray = message.content.split(' ')
-  let cmd = messageArray[0]
-  let args = messageArray.slice(1)
-  let dUser = message.mentions.users.first()
-  // Ping pong!
-  if (cmd === `${prefix}ping`) {
-    message.channel.send('pong')
-    return
-  }
-  // Youtube music
-  if (cmd === `${prefix}yt`) {
-    if (ytdl.validateURL(args[0])) {
-      if (message.member.voiceChannel) {
-        message.member.voiceChannel.join()
-          .then(connection => {
-            message.channel.send('Successfully joined voice channel and will now play')
-            connection.playStream(ytdl(args[0]))
-          }).catch(console.log)
-      } else {
-        message.channel.send('You need to join an channel first!')
-      }
+    // Timeout check
+    if (talkedRecently.has(message.author.id)) { return }
+    // Add user to timeout
+    talkedRecently.add(message.author.id)
+    setTimeout(() => {
+        talkedRecently.delete(message.author.id)
+    }, 2500)
+    // Prevent bot from talking to itself
+    if (message.author.bot) return
+    if (message.channel.type === 'dm') return
+    // Set cmd and arrays
+    let messageArray = message.content.split(' ')
+    let cmd = messageArray[0]
+    let args = messageArray.slice(1)
+    let dUser = message.mentions.users.first()
+    // Ping pong!
+    if (cmd === `${prefix}ping`) {
+        message.channel.send('pong')
+        return
     }
-    if (!args[0]) {
-      message.channel.send('Please provide a Youtube link')
-      return
+    // Youtube music
+    if (cmd === `${prefix}yt`) {
+        if (ytdl.validateURL(args[0])) {
+            if (message.member.voiceChannel) {
+                message.member.voiceChannel.join()
+                    .then(connection => {
+                        message.channel.send('Successfully joined voice channel and will now play')
+                        connection.playStream(ytdl(args[0]))
+                    }).catch(console.log)
+            } else {
+                message.channel.send('You need to join an channel first!')
+            }
+        }
+        if (!args[0]) {
+            message.channel.send('Please provide a Youtube link')
+            return
+        }
+        if (args[0] === 'stop') {
+            if (message.guild.voiceConnection) {
+                message.member.voiceChannel.leave()
+                message.channel.send('Leaving voice channel')
+            } else {
+                message.channel.send('I have already left')
+            }
+        }
     }
-    if (args[0] === 'stop') {
-      if (message.guild.voiceConnection) {
-        message.member.voiceChannel.leave()
-        message.channel.send('Leaving voice channel')
-      } else {
-        message.channel.send('I have already left')
-      }
+    // Avatar commands
+    if (cmd === `${prefix}avatar`) {
+        if (args[0]) {
+            message.channel.send(dUser.avatarURL)
+        } else {
+            message.channel.send(message.author.avatarURL)
+        }
     }
-  }
-  // Avatar commands
-  if (cmd === `${prefix}avatar`) {
-    if (args[0]) {
-      message.channel.send(dUser.avatarURL)
-    } else {
-      message.channel.send(message.author.avatarURL)
+    // Gif Commands
+    if (cmd === `${prefix}gif`) {
+        if (args[0] === 'random') {
+            Giphy.random({
+                rating: 'g',
+                fmt: 'json'
+            }, function (res) {
+                message.channel.send(res.data.url)
+                console.log(res.data.url)
+            })
+        } else if (args[0] === 'search') {
+            Giphy.random({
+                tag: args[1],
+                rating: 'g',
+                fmt: 'json'
+            }, function (res) {
+                message.channel.send(res.data.url)
+                console.log(res.data.url)
+            })
+        }
     }
-  }
-  // Gif Commands
-  if (cmd === `${prefix}gif`) {
-    if (args[0] === 'random') {
-      Giphy.random({
-        rating: 'g',
-        fmt: 'json'
-      }, function (res) {
-        message.channel.send(res.data.url)
-        console.log(res.data.url)
-      })
-    } else if (args[0] === 'search') {
-      Giphy.random({
-        tag: args[1],
-        rating: 'g',
-        fmt: 'json'
-      }, function (res) {
-        message.channel.send(res.data.url)
-        console.log(res.data.url)
-      })
-    }
-  }
 })
 // Bot Login
 bot.login(token)
+
+function haveVars() {
+
+    var valid = true;
+
+    //Make sure we have the required enviroment vars
+    if (!process.env.DISCORD_KEY) {
+        console.log('Please Provide a Discord API Token "DISCORD_KEY" in enviroment varibles ')
+        valid = false;
+    }
+    if (!process.env.GIPHY_KEY) {
+        console.log('Please Provide a prefic "PREFIX" in enviroment varibles ')
+        valid = false;
+    }
+    //Not end of the world use default
+    if (!process.env.PREFIX) {
+        console.log('Please Provide a prefic "PREFIX" in enviroment varibles; Using ";;" for default ')
+    }
+
+    return valid;
+}
